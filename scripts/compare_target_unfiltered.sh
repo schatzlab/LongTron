a592e402a32b0efb294bcfde77b3e12155ccfb0f
Chris Wilks <broadsword@gmail.com>
2018-05-22 12:15:41 -0400

    added 2,5,7 as intervals

--- scripts/compare_target_unfiltered.sh
+++ scripts/compare_target_unfiltered.sh
@@ -4,7 +4,7 @@
 target_f=$1
 target_wc=`wc -l $target_f | cut -d" " -f 1`
 echo "lr_min_reads	target_min_reads	matches	all_lr_jxs	all_target_jxs	lr_recall	target_recall" > compare.tsv
-for i in 1 10 20 50 100 500 1000
+for i in 1 2 5 7 10 15 20 50 100 500 1000
 do
 	/bin/bash -x ./compare_junctions_target_unfiltered.sh NA12878.bam.perl.introns.merged.sorted $i $target_f $target_wc >> compare.tsv
 done
