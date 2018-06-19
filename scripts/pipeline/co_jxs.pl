2364b7eb3476ceab579050a136820eb0683e19a0
Chris Wilks <broadsword@gmail.com>
2018-06-19 17:02:26 -0400

    added explanatory comments to co-occurring jxs script

--- scripts/pipeline/co_jxs.pl
+++ scripts/pipeline/co_jxs.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+#find co-occurring jx/exons and print shared read (nro) counts as well as indiviudal (nr) counts
 use strict;
 use warnings;
 
@@ -12,6 +13,9 @@ sub main
 	my %r2jx;
 	my %jxs;
 	my $jx_id = 1;
+	#first map:
+	#1) read_id to jx_ids
+	#2) jx_id to jx info
 	while(my $line=<IN>)
 	{
 		chomp($line);
@@ -22,7 +26,9 @@ sub main
 		$jx_id++;
 	}
 	close(IN);
+	# to actually get counts of all co-occurring jx for every jx
 	open(IN,"<$jx2readsF");
+	#now loop back through file
 	$jx_id = 1;
 	while(my $line=<IN>)
 	{
@@ -30,19 +36,22 @@ sub main
 		my ($c,$s,$e,$o,$nr,$reads) = split(/\t/,$line);
 		my @reads = split(/,/,$reads);
 		my %ojxs;
+		#find every co-occurring jx from every read this jx occurs in
 		for my $r (@reads)
 		{
 			my @ojxs = @{$r2jx{$r}};
 			for my $ojx (@ojxs)
 			{
 				next if($ojx == $jx_id);
+				#track count of every shared read this other jx occurs in
 				$ojxs{$ojx}++;
 			}
 		}
-		#now print mapping of this jx to all of its co-occuring jxs, sorted by the number of
+		#now print mapping of this jx to all of its co-occuring jxs, sorted by the # of
 		#reads it co-occurs in
 		print "".scalar(keys %ojxs)."\t";
 		print "".join("\t",($c,$s,$e,$o,$nr));
+		#sort by # of reads each co-occurring jx appears in
 		for my $ojx (sort { $ojxs{$a} <=> $ojxs{$b} } keys %ojxs)
 		{
 			my $nro = $ojxs{$ojx};
@@ -54,8 +63,3 @@ sub main
 	}
 	close(IN);
 }
-
-
-
-		
-
