e7fa8e7d7b586a0318ddc2d3347fd7a53e229f29
Chris Wilks <broadsword@gmail.com>
2018-06-26 10:36:27 -0400

    script to measure # of novel introns at different wiggle distances

---
+++ scripts/pipeline/wiggle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BEDTOOLS='/data/bedtools2/bin/bedtools'
+
+WIGGLE=${1}
+cat pre_novel_junctions | perl -ne 'BEGIN { $w='${WIGGLE}'; } chomp; $f=$_; ($c,$s,$e,$o,$nr,$reads,$pac,$nlr,$dist,$junctions)=split(/\t/,$f); $s1=$s-($w+1); $s2=$s+$w; $e1=$e-($w+1); $e2=$e+$w; print "".join("\t",($c,$s1,$s2,$o,$nr,$reads,$pac,$nlr,$dist,"$s-$e",0,$junctions))."\n"; print "".join("\t",($c,$e1,$e2,$o,$nr,$reads,$pac,$nlr,$dist,"$s-$e",1,$junctions))."\n";' | sort -k1,1 -k2,2n -k3,3n > pre_novel_junctions.split_ends.${WIGGLE}.bed
+
+#find non-overlaps with list of all short read junctions looking for ends separately (to allow for distant containments/overlaps)
+$BEDTOOLS intersect -sorted -a pre_novel_junctions.split_ends.${WIGGLE}.bed -b <(zcat ../../gtex_sra_junctions.split.bed.bgz) -v | perl -ne 'BEGIN { %h; } chomp; $f=$_; ($c,$c1,$c2,$o,$nr,$reads,$pac,$nlr,$dist,$coord,$t,$junctions)=split(/\t/,$f); $tnot=!$t; $k=$coord."-".$tnot; if($h{$k}) { print "".$h{$k}."\n"; delete($h{$k}); next; } $k=$coord."-".$t; $coord=~/^(\d+)-(\d+)$/; $c1=$1; $c2=$2; $h{$k}=join("\t",($c,$c1,$c2,$o,$nr,$reads,$pac,$nlr,$dist,$junctions));' > novel_junctions_bedtools.wiggle.${WIGGLE}
+
+egrep -v -e '(_alt)|(_random)|(_decoy)' novel_junctions_bedtools.wiggle.${WIGGLE} > novel_junctions_bedtools.wiggle.${WIGGLE}.no_alts_randoms_decoys
