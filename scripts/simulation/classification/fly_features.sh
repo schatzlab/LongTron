b32d4f169e781b6ebaec30016b0204e5edd6a9fb
Chris Wilks <broadsword@gmail.com>
2018-11-27 18:16:42 -0500

    added fly & worm features

---
+++ scripts/simulation/classification/fly_features.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#used to add sequence overlap features to a set of intervals (transcript, reads, etc...)
+#this is primarily used for classification in terms of long read alignment error profiling
+#features listed below should have already been preped separately (via prep_features.sh)
+
+#Features (not in order):
+#1) Splice motif count
+#2) # of exons
+#3) size of exons
+#4) size of introns
+#5) # of overlapping reads/transcripts
+#6) # of common 150 SNPs overlapping
+
+#samtools and bedtools should be in PATH
+ST=samtools
+BT=bedtools
+PERBASE=/data3/LongReadRNA/scripts/simulation/classification/perbase
+#the following needs to have had samtools faidx run on it ahead of time
+GENOME_INDEX=dmel-all-chromosome-r6.23_.fasta
+GENOME_SIZES=R623.fly.chrom.sizes
+
+SOURCE_PATH=./sources
+
+GC=$SOURCE_PATH/gc.bg
+RM=$SOURCE_PATH/rm.bed
+SM=$SOURCE_PATH/splices.bed.bgz
+EXONS_PERBASE=$SOURCE_PATH/exons.perbase.counts.bed.bgz
+TRANSCRIPTS_PERBASE=$SOURCE_PATH/transcripts.perbase.counts.bed.bgz
+LOCUS_STATS=$SOURCE_PATH/locus.stats.bed.nomt
+
+#TODO?
+#LOCAL_MAPPABILITY=$SOURCE_PATH/gv28.local_mappability.coords.bed
+#GC=$SOURCE_PATH/gc5Base.bg.clean
+#UMAP=$SOURCE_PATH/k24.Umap.MultiTrackMappability.sorted.bg
+#SEGDUPS=$SOURCE_PATH/segmental_dups_hg38.sorted
+
+BAM=$1
+
+#convert long read alignment BAM into BED of features (# exons, exon length, intron length, min_exon, min_intron, mapping_quality)
+#TODO: re-enable this for production
+#samtools view -F 2308 $BAM | cut -f 1,2,3,4,5,6,10 | perl -ne 'BEGIN { open(IN,"<class2transcript.all"); while($line=<IN>) { chomp($line); ($class,$transcript)=split(/\t/,$line); $class{uc($transcript)}=$class; } close(IN); $b=0; open(IN,"<'${GENOME_SIZES}'"); while($line=<IN>) { ($c,$sz)=split(/\t/,$line); $chrms{$c}=1; } close(IN); } chomp; ($name,$flag,$c,$s,$mapping_quality,$f,$seq)=split(/\t/,$_); next if(!$chrms{$c}); $n=$name; $n=~s/^([^_]+).*$/$1/; $c1=$class{uc($n)}; $rl=length($seq); $start=$s-1; my $o = (int($flag) & 0x10)?"-":"+"; $nexons=1; $exon_length=0; $intron_length=0; $r=$start; $pr=$r; $min_exon_sz=2**31; $min_intron_sz=2**31; while($f=~/(\d+)([NMD=X])/cg) { $i=$1; $t=$2; if($t eq "N") { $nexons++; $el=$r-$pr; $il=$i; $min_exon_sz=$el if($el < $min_exon_sz); $min_intron_sz=$il if($il < $min_intron_sz); $exon_length+=$el; $intron_length+=$il; $pr=$r+$i; } $r+=$i; } $el=$r-$pr; $exon_length+=$el; $min_exon_sz=$el if($el < $min_exon_sz); $min_intron_sz=0 if($nexons == 1); print "".join("\t",($c,$start,$r,"r$b",0,$o,$n,$c1,$rl,$nexons,$exon_length,$intron_length,$min_exon_sz,$min_intron_sz,$mapping_quality))."\n"; $b++;' | sort -k1,1 -k2,2n -k3,3n > ${BAM}.bed.rl.nX3.minX2.mq
+
+IN=${BAM}.bed.rl.nX3.minX2.mq
+
+OFFSET=`head -1 ${IN} | tr \\\\t \\\\n | wc -l`
+
+###RepeatMasker
+#overlap with RepeatMasker, now we just get total overlap
+$BT intersect -sorted -s -wao -a ${IN} -b $RM | perl -ne 'BEGIN { $bc=0; } chomp; @f=split(/\t/,$_); @f1=splice(@f,0,'$OFFSET'); $t=$f1[3]; $all=join("\t",@f1); if($t ne $pt) { if($pt) { print "$p\t$bc\n"; } $bc=0; $p=$all; } $pt=$t;  $rn=$f[3]; $rt=$f[6]; $bc+=$f[7]; END { if($pt) { print "$p\t$bc\n"; } }' > ${IN}.rm
+
+IN=${IN}.rm
+
+###Count of overlapping transcripts/reads *on the same strand*, always has at least 1 (itself)
+$BT intersect -sorted -s -c -a ${IN} -b ${IN} > ${IN}.ot
+
+###exon density
+cat ${IN}.ot | $PERBASE -c $GENOME_SIZES -f <(zcat $EXONS_PERBASE) > ${IN}.ot.ed
+
+###transcript density
+cat ${IN}.ot.ed | $PERBASE -c $GENOME_SIZES -f <(zcat $TRANSCRIPTS_PERBASE) > ${IN}.ot.ed.td
+
+###Logs of nexons, exon bp, intron bp
+cat ${IN}.ot.ed.td | perl -ne 'chomp; $f=$_; @f=split(/\t/,$f); ($ne,$ebp,$ibp)=($f[8],$f[9],$f[10]); $ibpl=($ibp>0?log($ibp):0); printf("%s\t%.3f\t%.3f\t%.3f\n",$f,log($ne),log($ebp),$ibpl);' > ${IN}.ot.ed.td.logsX3
+
+###SpliceMotif frequency
+#this is fixed now
+cat ${IN}.ot.ed.td.logsX3 | $PERBASE -c $GENOME_SIZES -f <(zcat $SM) -m -s 5 >  ${IN}.ot.ed.td.logsX3.sm
+
+###GC content
+cat ${IN}.ot.ed.td.logsX3.sm | $PERBASE -c $GENOME_SIZES -f $GC -t c > ${IN}.ot.ed.td.logsX3.sm.gc
+
+###Closest Locus transcript/exon stats (lengths, etc...), used at least for non-FL training/predictions
+$BT closest -g R623.fly.chrom.sizes.sorted -s -t first -D ref -a ${IN}.ot.ed.td.logsX3.sm.gc -b ${LOCUS_STATS} |  perl -ne 'chomp; $f=$_; @f=split(/\t/,$f); @f1=splice(@f,0,('$OFFSET'+9)); $all=join("\t",@f1); @f2=splice(@f,6); $all2=join("\t",@f2); print "$all\t".$f[4]."\t$all2\n";' > ${IN}.ot.ed.td.logsX3.sm.gc.lsX10
---
+++ scripts/simulation/classification/fly_prep_features.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#used to add sequence overlap features to a set of intervals (transcript, reads, etc...)
+#this is primarily used for classification in terms of long read alignment error profiling
+#assumes bedtools and samtools are in the PATH
+
+#Features:
+#1) Repeat Masker overlap
+#2) Tandem Repeat Finder (TRF) overlap (subset of #1)
+#3) Splice motif count
+#4) # of exons
+#5) size of exons
+#6) size of introns
+#7) # of overlapping reads/transcripts
+#8) # of common 150 SNPs overlapping
+
+ANNOT_VER=R623
+CHROM_SIZES=${ANNOT_VER}.fly.chrom.sizes
+NUM_CHRMS=25
+ANNOTATION=dmel-all-r6.23.gtf_.gz
+GENOME=dmel-all-chromosome-r6.23_.fasta
+RM=rmsk.txt.gz
+GC=gc5Base.bw
+
+BW2BG=/data/kent_tools/bigWigToBedGraph
+
+#get chrm sizes
+cat $GENOME | perl -ne 'chomp; $s=$_; if($s=~/^>/) { if($seq && length($seq) > 0) { print "\t".length($seq)."\n"; } $seq=""; $s=~s/^>([^\s]+).*$/$1/; print "$s"; next; } $seq.=$s; END { if($seq && length($seq) > 0) { print "\t".length($seq)."\n"; } }' > $CHROM_SIZES
+
+NUM_CHRMS=`wc -l $CHROM_SIZES | cut -d' ' -f 1`
+
+#pull out basic per-transcript/exon stats from annotation
+#this does NOT produce a BED file, coordinates are still 1-base
+zcat ${ANNOTATION} | egrep -e 'exon	' | sort -k1,1 -k4,4n -k5,5n > ${ANNOTATION}.exons
+
+#pull out annotated junctions
+cat ${ANNOTATION}.exons | cut -f 1,4,5,7,9 | perl -ne 'BEGIN { open(OUT,"| sort -k1,1 -k2,2n -k3,3n >'${ANNOTATION}'.exons.bed"); } chomp; $f=$_; ($c,$s,$e,$o,$info)=split(/\t/,$f); $info=~/transcript_id "([^"]+)";/; $t=$1; print OUT "$c\t".($s-1)."\t$e\t$t\t0\t$o\n"; if($ts{$t}) { $pe=$ts{$t}; $c1=$pe+1; $c2=$s-1;  print "$c\t$c1\t$c2\t$t\n"; } $ts{$t}=$e;' | sort -k1,1 -k2,2n -k3,3n | perl -ne 'chomp; $f=$_; ($c,$s,$e,$n)=split(/\t/,$f); if(!$ns{$n}) { $eid=0; } else { $eid=$ns{$n} } $eid++; print "$c\t$s\t$e\t$n\t$eid\n"; $ns{$n}=$eid; END { close(IN); }'  >  ${ANNOTATION}.junctions
+
+#get number of exon per transcript
+cat ${ANNOTATION}.exons | perl -ne 'chomp; $f=$_; $f=~/transcript_id "([^"]+)";/; $t=$1; print "$t\n";' | sort | uniq -c | sort -k1,1nr | perl -ne 'chomp; ($j,$c,$n)=split(/\s+/,$_); print "".uc($n)."\t$c\n";' > ${ANNOTATION}.transcripts_exon_count
+
+#Repeat Makser & SNPs prep
+#sort, collapse overlaps by strand, and then sort the combined file again
+zcat ${RM} | cut -f 6-8,10-12 | sort -k1,1 -k2,2n -k3,3n > ${RM}.sorted
+for sign in '\+' '-';
+do
+	egrep -e "	${sign}	" ${RM}.sorted | perl -ne 'chomp; $f=$_; ($c,$s,$e,$o,$n,$t)=split(/\t/,$f); $c=~/^chr//; if($p && $pc eq $c && $s <= $pe) { $p.=";$n"; $pt.=";$t"; $pe=$e; next; } if($p) { print "$pc\t$ps\t$pe\t$po\t$p\t$pt\n";} $p=$n; $pt=$t; $pc=$c; $ps=$s; $pe=$e; $po=$o; END { if($p) { print "$pc\t$ps\t$pe\t$po\t$p\t$pt\n";  } }' > ${RM}.strand
+done
+cat ${RM}.strand | sort -k1,1 -k2,2n -k3,3n | perl -ne 'chomp; @f=split(/\t/,$_); ($c,$s,$e)=splice(@f,0,3); $s--; print "".join("\t",($c,$s,$e))."\t".join("\t",@f)."\n";' > rm.bed
+
+#prep GC/UMAP track data by removing non-cannonical chromosomes and sort
+#assume we're getting BedGraph files (bigWigToBedGraph has already been run)
+$BW2BG $GC ${GC}.bg
+#cat ${GC}.bg | perl -ne 'chomp; $f=$_; @f=split(/\t/,$_); next if($f[0]=~/(_random)|(_alt)|(chrUn)/i); print "$f\n";' | sort -k1,1 -k2,2n -k3,3n > ${GC}.clean.sorted.bg
+mv ${GC}.bg ${GC}.clean.sorted.bg
+
+#create perbase for exon/transcript densities
+#this assumes that there are multiple overlapping exons in some regions (maybe even duplicates across transcripts)
+#these are big files, so have the ANNOTATION file sitting on a large filesystem with plenty of free space
+zcat $ANNOTATION | egrep 'exon	' | cut -f 1,4,5,7 | sort -k1,1 -k2,2n -k3,3n | perl -ne 'BEGIN { open(OUT,">'${ANNOTATION}'.exons.sorted.bed"); } chomp; $f=$_; ($c,$s,$e,$o)=split(/\t/,$f); print OUT "$c\t".($s-1)."\t$e\n"; if(!$pc || ($pc ne $c || $s > $pe)) { if($pc) { $ps--; for $i ($ps..($pe-1)) { print "$pc\t$i\t".($i+1)."\n"; }} $ps=$s; $pc=$c; $pe=$e; $po=$o; next; } $pe=$e; END { if($pc) { $ps--; for $i ($ps..($pe-1)) { print "$pc\t$i\t".($i+1)."\n"; } } close(OUT);}' | bgzip > ${ANNOTATION}.exons.perbase.bgz
+
+bedtools intersect -sorted -c -a <(zcat ${ANNOTATION}.exons.perbase.bgz) -b <(sort -k1,1 -k2,2n -k3,3n ${ANNOTATION}.exons.sorted.bed) | bgzip > ${ANNOTATION}.exons.perbase.counts.bgz
+
+zcat $ANNOTATION | egrep -e '	(\w+RNA)|(pseudogene)	' | cut -f 1,4,5,7 | sort -k1,1 -k2,2n -k3,3n | perl -ne 'BEGIN { open(OUT,">'${ANNOTATION}'.transcripts.sorted.bed"); } chomp; $f=$_; ($c,$s,$e,$o)=split(/\t/,$f); print OUT "$c\t".($s-1)."\t$e\n"; if(!$pc || ($pc ne $c || $s > $pe)) { if($pc) { $ps--; for $i ($ps..($pe-1)) { print "$pc\t$i\t".($i+1)."\n"; }} $ps=$s; $pc=$c; $pe=$e; $po=$o; next; } $pe=$e; END { if($pc) { $ps--; for $i ($ps..($pe-1)) { print "$pc\t$i\t".($i+1)."\n"; } } close(OUT);}' | bgzip > ${ANNOTATION}.transcripts.perbase.bgz
+
+bedtools intersect -sorted -c -a <(zcat ${ANNOTATION}.transcripts.perbase.bgz) -b <(sort -k1,1 -k2,2n -k3,3n ${ANNOTATION}.transcripts.sorted.bed) | bgzip > ${ANNOTATION}.transcripts.perbase.counts.bgz
+
+#extract out all cannonical splice motifs (forward + reverese) from reference across all main chromosomes
+#takes ~33m22.814s for HG38
+(head -${NUM_CHRMS} $CHROM_SIZES | cut -f 1 | sort | perl -ne 'BEGIN { %F=("GT"=>1,"gt"=>1,"Gt"=>1,"gT"=>1,"AG"=>1,"ag"=>1,"Ag"=>1,"aG"=>1); %R=("CT"=>1,"AC"=>1,"cT"=>1,"Ct"=>1,"ct"=>1,"Ac"=>1,"aC"=>1,"ac"=>1); } chomp; $chrm=$_; @s=`samtools faidx '${GENOME}' $chrm | fgrep -v ">"`; chomp(@s); $seq=join("",@s); $len=length($seq); for($i=0;$i<($len-1);$i++) { $m=substr($seq,$i,2); $b="$chrm\t$i\t".($i+1)."\t$m\n"; if($F{$m}) { print $b; } if($R{$m}) { print STDERR $b; } }' | bgzip > ${ANNOT_VER}.forward_splice_motifs.all.tsv.bgz) 2>&1 | bgzip > ${ANNOT_VER}.reverse_splice_motifs.all.tsv.bgz
+
+#merge forward and reverse strands of all cannonical splice motifs from reference into one file (sorted)
+cat <(zcat ${ANNOT_VER}.forward_splice_motifs.all.tsv.bgz | perl -ne 'chomp; print "$_\t0\t+\n";') <(zcat ${ANNOT_VER}.reverse_splice_motifs.all.tsv.bgz | perl -ne 'chomp; print "$_\t0\t-\n";') | sort -k1,1 -k2,2n -k3,3n | bgzip > ${ANNOT_VER}.splice_motifs.all.tsv.bgz
+
+
+#get per-gene level stats about transcript and exon lengths/counts/sums/averages
+zcat $ANNOTATION | egrep -e 'exon	' | perl -ne 'chomp; $f=$_; @f=split(/\t/,$f); ($ch,$start,$end,$strand)=($f[0],$f[3],$f[4],$f[6]); $f=~/gene_id "([^"]+)"/; $g=$1; $h2{$g}->[0]=$start if(!$h2{$g}->[0] || $start < $h2{$g}->[0]); $h2{$g}->[1]=$end if(!$h2{$g}->[1] || $end > $h2{$g}->[1]); $h2{$g}->[2]=$ch; $h2{$g}->[3]=$strand; $len=($end-$start)+1; $f=~/transcript_id "([^"]+)"/; $t=$1; push(@{$h{$g}->{$t}}, $len); END { for $g (keys %h) { ($st,$en,$chrm,$str)=@{$h2{$g}}; $sum=0; $min=(2**32)-1; $max=0; $mine=(2**32)-1; $maxe=0; $counte=0; $count=0; map { $i=0; for $e (@{$h{$g}->{$_}}) { $i+=$e; $mine=$e if($e < $mine); $maxe=$e if($e > $maxe); $counte++; } $count++; $sum+=$i; $max=$i if($i > $max); $min=$i if($i < $min); } (keys %{$h{$g}}); $st--; printf("$chrm\t$st\t$en\t$g\t$count\t$str\t$sum\t$min\t$max\t%.3f\t$counte\t$mine\t$maxe\t%.3f\n",($sum/$count),($sum/$counte));}}' | sort -k1,1 -k2,2n -k3,3n > ${ANNOTATION}.exons.stats.bed
