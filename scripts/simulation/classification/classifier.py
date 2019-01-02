3650c9fcff548d055fc85007f306a7f21b284450
Chris Wilks <broadsword@gmail.com>
2019-01-02 18:34:23 -0500

    correcting model file symlink creation in classifier

--- scripts/simulation/classification/classifier.py
+++ scripts/simulation/classification/classifier.py
@@ -57,5 +57,5 @@ sys.stdout.write("importance: ")
 [sys.stdout.write(" %s:%s " % (fields[i],str(x_))) for (i,x_) in enumerate(bc.feature_importances_)]
 sys.stdout.write("\n")
 
-os.symlink("./trained_models/%s.rfc_4_trained" % (fname),'./trained_models/short.rfc_4_trained')
-os.symlink("./trained_models/%s.rfc_2_trained" % (fname),'./trained_models/short.rfc_2_trained')
+os.symlink("./%s.rfc_4_trained" % (fname),'./trained_models/short.rfc_4_trained')
+os.symlink("./%s.rfc_2_trained" % (fname),'./trained_models/short.rfc_2_trained')
