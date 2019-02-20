51ae8736283866cbcc7258f0653b789bf9c73fbf
ChristopherWilks <broadsword@gmail.com>
2019-02-20 13:10:07 -0500

    added simple extraction of splice motifs to perbase

--- scripts/simulation/classification/perbase.cpp
+++ scripts/simulation/classification/perbase.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <cstdint>
 #include <fcntl.h>
+//#include <stdio.h>
 #include <map>
 
 static const int ASCII_OFFSET=48;
@@ -24,6 +25,7 @@ static const int LINE_BUFFER_LENGTH=1048576;
 //For future use
 static int TEST_MODE = -1;
 static int SPLICE_MOTIF_MODE = 0;
+int GENOME_BASE_FLAG=100;
 
 typedef std::map<std::string, int> chrm_map;
 
@@ -72,23 +74,72 @@ int parse_chrm_idx(const char* chrm_id)
 	return idx;
 }
 
+template <typename T>
+int read_chromosome_string(FILE* cfp, T* chrm_array, long chrm_size, bool first = false) { }
+
+//chromosomes in the FASTA file *must* be in the same order as the sizes file
+template<>
+int read_chromosome_string<char>(FILE* cfp, char* chrm_array, long chrm_size, bool first)
+{
+	size_t length=-1;
+    char* buf = new char[2048];
+    ssize_t bytes_read = 0;
+    if(first) 
+    { 
+	    bytes_read = getline(&buf, &length, cfp);
+        if(bytes_read == -1) {
+            fprintf(stderr,"no lines in chromosome fasta file, exiting\n");
+            exit(-1);
+        }
+    }
+	bytes_read = getline(&buf, &length, cfp);
+    ssize_t total_bytes_read = 0;
+    char* chrm_array_ptr = chrm_array;
+    while(bytes_read != -1) {
+        //hit the next header
+        if(buf[0] == '>')
+            break;
+        //track size of sequence, minus new lines
+        total_bytes_read += bytes_read-1;
+        memmove(chrm_array_ptr, buf, bytes_read-1);
+        chrm_array_ptr = (chrm_array_ptr + bytes_read)-1;
+	    bytes_read = getline(&buf, &length, cfp);
+    }
+    delete buf;
+    return total_bytes_read;
+}
+
+//will only read the chromosomes from the fasta up to the ones included in the sizes file
 template<typename T>
-T** build_chromosome_array(std::string chrm_file, chrm_map* chrm_name2id)
+T** build_chromosome_array(std::string chrm_file, chrm_map* chrm_name2id, std::string chrm_fasta_file = NULL)
 {
 	std::ifstream infile(chrm_file.c_str());
+    FILE* chrm_fasta_file_fp = NULL;
+    if(!chrm_fasta_file.empty())
+        chrm_fasta_file_fp = fopen(chrm_fasta_file.c_str(), "r");
 	assert(infile);
 	std::string line;
 	T** chrm_array = new T*[NUM_CHRM+1];
 	std::vector<std::string> tokens;
 	int chrm_idx = 0;
+    int bytes_read = 0;
 	while(getline(infile, line))
 	{
 		split_string(line, '\t', &tokens);
 		long chrm_size = atol(tokens.at(CHRM_SIZE_COL).c_str());
+        //if reading the actual chromosome sequence, add a slot for the null terminator
+        if(chrm_fasta_file_fp)
+            chrm_size += 1;
 		chrm_array[chrm_idx] = build_array<T>(chrm_size);
+        if(chrm_fasta_file_fp) {
+            bytes_read = read_chromosome_string(chrm_fasta_file_fp, chrm_array[chrm_idx], chrm_size, chrm_idx == 0);
+            fprintf(stderr,"%s\t%d\tread\n",tokens.at(CHRM_COL).c_str(), bytes_read);
+        }
 		chrm_name2id->emplace(tokens.at(CHRM_COL), chrm_idx++);
 	}
 	infile.close();
+    if(chrm_fasta_file_fp)
+        fclose(chrm_fasta_file_fp);
 	return chrm_array;
 }
 
@@ -128,8 +179,13 @@ void set_value(int cidx, long start, long end, char strand, T value, T** chrm_ar
 	}
 }
 
-void output_line(char* line, double summary) { fprintf(stdout,"%s\t%.3f\n", line, summary); } 
-//void output_line(char* line, double summary) { fprintf(stdout,"%s\t%.0f\n", line, summary); } 
+void output_line(char* line, double summary, char* motif) 
+{
+    if(motif != NULL)
+        fprintf(stdout,"%s\t%s\n", line, motif); 
+    else
+        fprintf(stdout,"%s\t%.3f\n", line, summary); 
+} 
 
 //about 3x faster than the sstring/string::getline version
 template <typename T>
@@ -182,11 +238,18 @@ T process_line(char* line, char* delim, int* cidx, long* start, long* end, char*
 }
 
 template <typename T>
-double summarize_region(int* cidx, long* start, long* end, char* strand, T** chrm_array, int strand_col)
+double summarize_region(int* cidx, long* start, long* end, char* strand, T** chrm_array, int strand_col, char** motif)
 {
 	double summary = 0.0;
 	long length = *end - *start;
 	int strand_val = *strand=='+'?1:3;
+    //just extract splice motifs
+    if(strand_col == GENOME_BASE_FLAG)
+    {
+            //adjust for the base-0 array, assumes base-1 coordinates (not BED)
+			sprintf(*motif, "%c%c-%c%c", chrm_array[*cidx][(*start)-1], chrm_array[*cidx][*start], chrm_array[*cidx][(*end)-2], chrm_array[*cidx][(*end)-1]);
+            return summary;
+    }
 	//fprintf(stderr,"value: %d line: %c\n",strand_val,*strand);
 	for(int i=*start; i < *end; i++)
 	{
@@ -210,10 +273,12 @@ double summarize_region(int* cidx, long* start, long* end, char* strand, T** chr
 }
 
 template <typename T>
-void go(std::string chrm_file, std::string perbase_file, int strand_col)
+void go(std::string chrm_file, std::string perbase_file, int strand_col, std::string chrm_fasta_file = NULL)
 {
 	chrm_map chrm_name2id;
-	T** chrm_array = build_chromosome_array<T>(chrm_file, &chrm_name2id);
+	T** chrm_array = build_chromosome_array<T>(chrm_file, &chrm_name2id, chrm_fasta_file);
+    //if(strand_col == 100)
+    //    return;
 	int cidx;
 	int pcidx = -1;
 	long start, end;
@@ -222,38 +287,45 @@ void go(std::string chrm_file, std::string perbase_file, int strand_col)
 	
 	char* line = new char[LINE_BUFFER_LENGTH];
 	size_t length = LINE_BUFFER_LENGTH;
-	FILE* fin = fopen(perbase_file.c_str(), "r");
-	assert(fin);
-	ssize_t bytes_read = getline(&line, &length, fin);
-	int err;
-	while(bytes_read != -1)
-	{
-		err = 0;
-		//assumes no header
-		T value = process_line<T>(strdup(line), "\t", &cidx, &start, &end, &strand, VALUE_COL, strand_col, &chrm_name2id, &err);
-		if(err == 0)
-		{
-			if(cidx != pcidx && pcidx != -1)
-			{
-				fprintf(stderr,"BUILDING: chr idx %d done\n",pcidx);
-				//only do chr1 and 10 for testing
-				/*if(pcidx == 10)
-					break;*/
-			}
-			pcidx = cidx;
-			set_value<T>(cidx, start, end, strand, value, chrm_array, strand_col);
-		}
-		bytes_read = getline(&line, &length, fin);
-	}
-	if(pcidx != -1)
-		fprintf(stderr,"BUILDING: chr idx %d done\n",pcidx);
-	std::cerr << "building genome wide value array done\n";
+    ssize_t bytes_read = -1;
+    int err;
+    //if doing a 2-file perbase counting operation
+    if(strand_col != GENOME_BASE_FLAG)
+    {
+        FILE* fin = fopen(perbase_file.c_str(), "r");
+        assert(fin);
+        bytes_read = getline(&line, &length, fin);
+        while(bytes_read != -1)
+        {
+            err = 0;
+            //assumes no header
+            T value = process_line<T>(strdup(line), "\t", &cidx, &start, &end, &strand, VALUE_COL, strand_col, &chrm_name2id, &err);
+            if(err == 0)
+            {
+                if(cidx != pcidx && pcidx != -1)
+                {
+                    fprintf(stderr,"BUILDING: chr idx %d done\n",pcidx);
+                    //only do chr1 and 10 for testing
+                    /*if(pcidx == 10)
+                        break;*/
+                }
+                pcidx = cidx;
+                set_value<T>(cidx, start, end, strand, value, chrm_array, strand_col);
+            }
+            bytes_read = getline(&line, &length, fin);
+        }
+        if(pcidx != -1)
+            fprintf(stderr,"BUILDING: chr idx %d done\n",pcidx);
+        std::cerr << "building genome wide value array done\n";
+    }
 	pcidx = -1;
 	//now read main file from STDIN line-by-line
 	//FILE* fin1 = fopen("q1", "r");
 	//bytes_read = getline(&line, &length, fin1);
 	bytes_read = getline(&line, &length, stdin);
 	char* line_wo_nl = new char[LINE_BUFFER_LENGTH];
+    //only used when extracting splice motifs
+    char* motif = new char[4];
 	while(bytes_read != -1)
 	{
 		//get rid of newline
@@ -267,8 +339,8 @@ void go(std::string chrm_file, std::string perbase_file, int strand_col)
 			if(cidx != pcidx && pcidx != -1)
 				fprintf(stderr,"MATCHING: chr idx %d done\n",pcidx);
 			pcidx = cidx;
-			double summary = summarize_region<T>(&cidx, &start, &end, &strand, chrm_array, strand_col);
-			output_line(line_wo_nl, summary);
+			double summary = summarize_region<T>(&cidx, &start, &end, &strand, chrm_array, strand_col, &motif);
+			output_line(line_wo_nl, summary, motif);
 		}
 		//bytes_read = getline(&line, &length, fin1);
 		bytes_read = getline(&line, &length, stdin);
@@ -287,18 +359,28 @@ int main(int argc, char* argv[])
 	std::string perbase_file;
 	std::string chrm_file;
 	std::string perbase_type = "int";
+    std::string chrm_fasta_file;
 	int strand_col = -1;
-	while((o  = getopt(argc, argv, "f:t:c:s:m")) != -1) 
+	while((o  = getopt(argc, argv, "f:t:c:s:g:m")) != -1) 
 	{
 		switch(o) 
 		{
 			case 'c': chrm_file = optarg; break;
 			case 'f': perbase_file = optarg; break;
 			case 't': perbase_type = optarg; break;
+			case 'g': chrm_fasta_file = optarg; break;
 			case 's': strand_col = atoi(optarg); break;
 			case 'm': SPLICE_MOTIF_MODE = 1; break;
 		}
 	}
+    //chromosomes in the FASTA file *must* be in the same order as the sizes file (chrm_file)
+    //also input coordinates *must* be base-1 not BED or base-0
+    if(!chrm_fasta_file.empty())
+    {
+	    std::cerr << "hello" << " " << perbase_file << " " << chrm_file << " " << chrm_fasta_file << "\n";
+        go<char>(chrm_file, perbase_file, GENOME_BASE_FLAG, chrm_fasta_file); 
+        return 0;
+    }
 	if(SPLICE_MOTIF_MODE && strand_col == -1)
 	{
 		std::cerr << "Missing strand column parameter (e.g. -s 5) for splice motif mode" << "\n";
