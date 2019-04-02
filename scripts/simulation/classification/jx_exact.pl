4bd3573eee75d64148c8abde8aa9b0a17d7486df
ChristopherWilks <broadsword@gmail.com>
2019-04-02 16:21:53 -0400

    working exact jx matching script

--- scripts/simulation/classification/jx_exact.pl
+++ scripts/simulation/classification/jx_exact.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/env perl
+#takes a full list of snaptron or annotated junctions and matches them using exact coordinate matches
+#to the set passed in as a file on the command line
 use strict;
 use warnings;
 
@@ -20,32 +22,36 @@ while(my $f=<IN>)
     map { my ($id)=split(/\./,$_); $totals{$id}++; } split(/,/,$f[2]);
     $h{$k}=\@f;
     $h{$k}->[3]=0;
+    $h{$k}->[4]=0;
     $small_count++;
 } 
 close(IN);
 my $matching=0; 
+#read snaptron/annotation
 while(my $z=<STDIN>)
 {
     chomp($z); 
-    my ($c,$s,$e)=split(/\t/,$z);
+    my ($c,$s,$e,$o,$a)=split(/\t/,$z);
     my $k = join("\t",($c,$s,$e)); 
     #print "coord\t$z\t".join("\t",@{$h{$z}})."\n"; 
     if($h{$k})
     {
-        $h{$k}->[3] += 1; 
-        $matching++;
+        $h{$k}->[3] += 1;
+        $h{$k}->[4] = 1 if($a != 0);
     }
 }
-#large_count will vary with the total number of matching recount jxs (it's not the total number of recount jxs)
-print STDERR "".($matching/$small_count)."\t$matching\t$small_count\n";
 my %h2;
+my $num_annotated = 0;
 #assign counts to read/transcript IDs
-for my $a (keys %h) 
+for my $k (keys %h) 
 { 
-    my $c = $h{$a}->[3]; 
+    my $c = $h{$k}->[3]; 
+    my $a = $h{$k}->[4];
     if($c && $c > 0) 
     { 
-        my @f1=split(/,/,$h{$a}->[2]); 
+        $matching++;
+        $num_annotated++ if($a != 0);
+        my @f1=split(/,/,$h{$k}->[2]); 
         for my $e (@f1) 
         { 
             my ($id)=split(/\./,$e);
@@ -53,6 +59,11 @@ for my $a (keys %h)
         } 
     } 
 } 
+my $novel = $small_count - $matching;
+my $snaptron_unannotated = $matching - $num_annotated;
+#printf("%.2f\t%d\t%d\t%.2f\t%d\t%d\t%.2f\t%d\t%d\n",($novel/$small_count),$novel,$small_count,($num_annotated/$small_count),$num_annotated,$small_count,($snaptron_unannotated/$small_count),$snaptron_unannotated,$small_count);
+printf("%d (100\%)\t%d (%.0f\%)\t%d (%.0f\%)\t%d (%.0f\%)\n",$small_count,$num_annotated,100*($num_annotated/$small_count),$matching,100*($matching/$small_count),$novel,100*($novel/$small_count));
+
 #now print count for each read/transcript ID
 #as well as ratio of splice matches to all splices in each TID
 for my $tid (keys %h2)
