94cb3a64f6cf4f37aee153c946c6b29b2baa503c
ChristopherWilks <broadsword@gmail.com>
2019-04-02 16:24:33 -0400

    removed commented-out split end comparison cruft

--- scripts/simulation/junction_wiggle.sh
+++ scripts/simulation/junction_wiggle.sh
@@ -24,31 +24,16 @@ cat $ANNOT_JUNCTIONS | perl -ne 'chomp; ($c,$s,$e,$t,$eid)=split(/\t/,$_); $s--;
 #totally novel with wiggle (no split ends complications)
 $BT window -a ${JUNCTIONS}.clean.bed -b ${ANNOT_JUNCTIONS}.bed -w $WIGGLE -v | perl -ne 'BEGIN { $w='${WIGGLE}'; open(IN,"<'${TID_FILE}'"); %tids; while($line=<IN>) { chomp($line); ($tid,$n)=split(/\t/,$line); $tids{$tid}=$n; } close(IN); } chomp; $f=$_; ($c1,$s1,$e1,$o,$nr,$reads)=split(/\t/,$f); %mtids; for $tid (split(/,/,$reads)) { ($tid,$eid)=split(/\./,$tid); $mtids{$tids{$tid}}=1; } print "$c1\t$s1\t$e1\t$o\t$nr\t$reads\t".(join(";",sort keys %mtids))."\n";' > sim10.junctions.clean.w${WIGGLE}.novel
 
-$BT window -a ${JUNCTIONS}.clean.bed -b ${ANNOT_JUNCTIONS}.bed -w $WIGGLE | perl -ne 'BEGIN { $w='${WIGGLE}'; open(IN,"<'${TID_FILE}'"); %tids; while($line=<IN>) { chomp($line); ($tid,$n)=split(/\t/,$line); $n=~s/_0_[+-]$//; $tids{$tid}=uc($n); } close(IN); %mtids; } chomp; $f=$_; ($c1,$s1,$e1,$o,$nr,$reads,$c2,$s2,$e2,$t,$eid)=split(/\t/,$f); $t=~s/_0_[+-]$//; $t=uc($t); $d1=abs($s1-$s2); $d2=abs($e1-$e2); $c3=($d1>$d2?$s1:$e1); $k="$c1\t$s1\t$e1"; $matched_tid=0; for $tid (split(/,/,$reads)) { ($tid,$eid2)=split(/\./,$tid); if($tids{$tid} eq $t && $eid eq $eid2 && $mtids{$k} != 2) { $matched_tid=1; $mtids{$k}=1; break; } }  $line=join("\t",($c1,$s1,$e1,$o,$nr,$reads,$c3)); if($d1 <= $w && $d2 <= $w) { $mtids{$k} = 2 if($matched_tid == 1); delete $h2{$k}; $h{$k}=$line; } else { if(!$h{$k}) { $h2{$k}=$line; } }  END { for $k (keys %h2) { $matched=$mtids{$k}; $matched=0 if(!$mtids{$k}); $k=$h2{$k}; print "$k\t$matched\n"; } for $k (keys %h) { $matched=$mtids{$k}; $matched=0 if(!$mtids{$k}); $k=$h{$k}; print STDERR "$k\t$matched\n"; }}' 2> sim10.junctions.clean.w${WIGGLE}.matching > sim10.junctions.clean.w${WIGGLE}.nonmatching
+#determine matching/nonmatching of junctions assigned to reads/transcripts
+#To match:
+#1) each end must be within window of it's annotated end, this decides which file it goes into (doesn't depend on #2)
+#2) at least one  of the aligned jx's reads must match the annotated transcript ID it's overlapping
+#and the exon/jx idx must match within the overlapping transcript, this decides if it gets a 1 or 0 for the matching column in output
 
-#UPDATE: don't do split ends for junctions
-#split annotated junctions to be both ends (but separate)
-#w/o window
-#cat $ANNOT_JUNCTIONS | perl -ne 'chomp; $f=$_; ($c,$s,$e)=split(/\t/,$f); $s1=$s-1; $s2=$s; $e1=$e-1; $e2=$e; print "".join("\t",($c,$s1,$s2))."\n"; print "".join("\t",($c,$e1,$e2))."\n";' | sort -k1,1 -k2,2n -k3,3n > $ANNOT_JUNCTIONS.split_ends.bed
 
-#split aligned junctions to be both ends (but separate)
-#w/o window
-#cat ${JUNCTIONS}.clean | perl -ne 'BEGIN { $w='${WIGGLE}'; } chomp; $f=$_; ($c,$s,$e,$o,$nr,$reads)=split(/\t/,$f); $s1=$s-1; $s2=$s; $e1=$e-1; $e2=$e; print "".join("\t",($c,$s1,$s2,$o,$nr,$reads,"$s-$e"))."\n"; print "".join("\t",($c,$e1,$e2,$o,$nr,$reads,"$s-$e"))."\n";' | sort -k1,1 -k2,2n -k3,3n > sim10.junctions.split_ends.bed
-
-#now check via ends
-#use split ends to check either side (more flexible)
-#$BT window -a sim10.junctions.split_ends.bed -b $ANNOT_JUNCTIONS.split_ends.bed -w $WIGGLE -v > sim10.junctions.split_ends.bed.w${WIGGLE}
-
-#sort -k1,1 -k2,2n -k3,3n sim10.junctions.split_ends.bed.w${WIGGLE} | uniq > sim10.junctions.split_ends.bed.w${WIGGLE}.sorted
-
-#but now require both ends have to be novel
-#cat sim10.junctions.split_ends.bed.w${WIGGLE}.sorted | perl -ne 'BEGIN { %h; } chomp; $f=$_; ($c,$c1,$c2,$o,$nr,$reads,$coord,$matching)=split(/\t/,$f); $k=$coord; if($h{$k}) { print "".$h{$k}."\n"; delete($h{$k}); next; } $k=$coord; $coord=~/^(\d+)-(\d+)$/; $c1=$1; $c2=$2; $h{$k}=join("\t",($c,$c1,$c2,$o,$nr,$reads,$matching));' | sort -k5,5nr -k1,1 -k2,2n -k3,3n > sim10.junctions.split_ends.bed.w${WIGGLE}.both_ends
-
-#either end can be novel, but print the end that we know *is* novel; also print the original (non-split, non-windowed) BED-formatted coordinates as primary, important for the rest of the steps
-#cat sim10.junctions.split_ends.bed.w${WIGGLE}.sorted | perl -ne 'BEGIN { %h; } chomp; $f=$_; ($c,$c1,$c2,$o,$nr,$reads,$coord,$matching)=split(/\t/,$f); $k=$coord; $coord=~/^(\d+)-(\d+)$/; $c3=$c1; $c1=$1; $c2=$2; $d1=abs($c1-$c3); $d2=abs($c2-$c3); $c3=($d1>$d2?$c2:$c1); if(!$h{$k}) { print "".join("\t",($c,--$c1,$c2,$o,$nr,$reads,$c3,$matching))."\n"; } $h{$k}=1;' | sort -k5,5nr -k1,1 -k2,2n -k3,3n > sim10.junctions.split_ends.bed.w${WIGGLE}.either_end
+$BT window -a ${JUNCTIONS}.clean.bed -b ${ANNOT_JUNCTIONS}.bed -w $WIGGLE | perl -ne 'BEGIN { $w='${WIGGLE}'; open(IN,"<'${TID_FILE}'"); %tids; while($line=<IN>) { chomp($line); ($tid,$n)=split(/\t/,$line); $n=~s/_0_[+-]$//; $tids{$tid}=uc($n); } close(IN); %mtids; } chomp; $f=$_; ($c1,$s1,$e1,$o,$nr,$reads,$c2,$s2,$e2,$t,$eid)=split(/\t/,$f); $t=~s/_0_[+-]$//; $t=uc($t); $d1=abs($s1-$s2); $d2=abs($e1-$e2); $c3=($d1>$d2?$s1:$e1); $k="$c1\t$s1\t$e1"; $matched_tid=0; for $tid (split(/,/,$reads)) { ($tid,$eid2)=split(/\./,$tid); if($tids{$tid} eq $t && $eid eq $eid2 && $mtids{$k} != 2) { $matched_tid=1; $mtids{$k}=1; break; } }  $line=join("\t",($c1,$s1,$e1,$o,$nr,$reads,$c3)); if($d1 <= $w && $d2 <= $w) { $mtids{$k} = 2 if($matched_tid == 1); delete $h2{$k}; $h{$k}=$line; } else { if(!$h{$k}) { $h2{$k}=$line; } }  END { for $k (keys %h2) { $matched=$mtids{$k}; $matched=0 if(!$mtids{$k}); $k=$h2{$k}; print "$k\t$matched\n"; } for $k (keys %h) { $matched=$mtids{$k}; $matched=0 if(!$mtids{$k}); $k=$h{$k}; print STDERR "$k\t$matched\n"; }}' 2> sim10.junctions.clean.w${WIGGLE}.matching > sim10.junctions.clean.w${WIGGLE}.nonmatching 
 
 ALL_NONMATCH=sim10.junctions.split_ends.bed.w${WIGGLE}.either_end.sorted
-#cat sim10.junctions.split_ends.bed.w${WIGGLE}.either_end sim10.junctions.clean.w${WIGGLE}.nonmatching | sort -k1,1 -k2,2n -k3,3n > $ALL_NONMATCH
 cat sim10.junctions.clean.w${WIGGLE}.nonmatching | sort -k1,1 -k2,2n -k3,3n > $ALL_NONMATCH
 
 #now separate into further categories those which have some form of overlap:
