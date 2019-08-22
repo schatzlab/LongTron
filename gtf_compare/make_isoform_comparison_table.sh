6e398d7848d708c7f58966cc024852515a53f93d
ChristopherWilks <broadsword@gmail.com>
2019-08-22 18:51:18 -0400

    fixed extra count of 1 in total from header

--- gtf_compare/make_isoform_comparison_table.sh
+++ gtf_compare/make_isoform_comparison_table.sh
@@ -2,9 +2,6 @@
 #/path/to/<comparison>.tmap file
 tmap=$1
 
-#cut -f 5 $tmap | sort -u > ${tmap}.unique_qs
-#total_q=`wc -l ${tmap}.unique_qs | cut -d' ' -f 1`
-
 cut -f 3 $tmap | sort | uniq -c > ${tmap}.counts
 #only want 1) multi-exon 2) full matching "=", contains matching ("c" for contained *in* ref, and "k" for contained in query)
 cut -f 2,3,5,6 $tmap | fgrep "	=	" | egrep -v -e '	1$' > ${tmap}.exact.multi
@@ -13,20 +10,14 @@ cut -f 2,3,5,6 $tmap | fgrep "	c	" | egrep -v -e '	1$' > ${tmap}.contained.multi
 #query contains ref
 cut -f 2,3,5,6 $tmap | fgrep "	k	" | egrep -v -e '	1$' > ${tmap}.contains.multi
 
-#cat ${tmap}.exact.multi ${tmap}.contains.multi ${tmap}.contained.multi > ${tmap}.both.multi
-
 #now get intergenic, completely novel multi-exon queries
 cut -f 2,3,5,6 $tmap | fgrep "	u	" | egrep -v -e '	1$' > ${tmap}.novel.multi
 cut -f 2,3,5,6 $tmap | fgrep "	p	" | egrep -v -e '	1$' >> ${tmap}.novel.multi
 
-#cut -f 5 ${tmap}.novel.multi | sort -u > ${tmap}.novel.multi.unique_qs
-
 #repeat regions
 cut -f 2,3,5,6 $tmap | fgrep "	r	" | egrep -v -e '	1$' > ${tmap}.repeats.multi
-#cut -f 5 ${tmap}.repeats.multi | sort -u > ${tmap}.repeats.multi.unique_qs
 
-#cut -f 5 ${tmap}.both.multi | sort -u > ${tmap}.both.multi.unique_qs
-total_q=`cut -f 5 $tmap | sort -u | wc -l | cut -d' ' -f 1`
+total_q=`cut -f 5 $tmap | tail -n+2 | sort -u | wc -l | cut -d' ' -f 1`
 matching_q=`cut -f 3 ${tmap}.exact.multi | sort -u | wc -l | cut -d' ' -f 1`
 contained_q=`cut -f 3 ${tmap}.contained.multi | sort -u | wc -l | cut -d' ' -f 1`
 contains_q=`cut -f 3 ${tmap}.contains.multi | sort -u | wc -l | cut -d' ' -f 1`
