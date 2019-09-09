b145913c653ac1f7fe5e5a298ccdd64c93513b8d
ChristopherWilks <broadsword@gmail.com>
2019-09-09 14:46:38 -0400

    no longer run symmetric filtering (-a)

--- gtf_compare/run_gcomf0.sh
+++ gtf_compare/run_gcomf0.sh
@@ -9,5 +9,7 @@ mkdir -p $dir
 cd $dir
 ln -fs ../${gtf1}
 ln -fs ../${gtf2}
-../gcom -a -T -r $gtf1 --fuzz-length 20 $gtf2 > run 2>&1
+../gcom -r $gtf1 --fuzz-length 0 $gtf2 > run 2>&1
+#../gcom -a -T -r $gtf1 --fuzz-length 0 $gtf2 > run 2>&1
+#../gcom_orig -r $gtf1 $gtf2 > run 2>&1
 cd ../
--- gtf_compare/run_gcomf20.sh
+++ gtf_compare/run_gcomf20.sh
@@ -9,5 +9,6 @@ mkdir -p $dir
 cd $dir
 ln -fs ../${gtf1}
 ln -fs ../${gtf2}
-../gcom -a -T -r $gtf1 --fuzz-length 20 $gtf2 > run 2>&1
+#../gcom -a -T -r $gtf1 --fuzz-length 20 $gtf2 > run 2>&1
+../gcom -r $gtf1 --fuzz-length 20 $gtf2 > run 2>&1
 cd ../
