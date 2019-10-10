16defbef487b80f412ae3948af0498822e46c7cd
ChristopherWilks <broadsword@gmail.com>
2019-10-10 12:22:20 -0400

    move counts table creation into main gcom run script

--- gtf_compare/collect_all_stats.sh
+++ gtf_compare/collect_all_stats.sh
@@ -2,11 +2,15 @@
 
 #bdir=$(dirname $0)
 compare_dir=$1
-comparison=`echo "$compare_dir" | perl -e 'chomp; $f=$_; @f=split(/\//,$f); $d=pop(@f);  print "$d\n";'`
+comparison=`echo "$compare_dir" | perl -ne 'chomp; $f=$_; @f=split(/\//,$f); $d=pop(@f);  print "$d\n";'`
 
-find $compare_dir -name "*.tmap" > ${comparison}.all_tmaps
+#find $compare_dir -name "*.tmap" > ${comparison}.all_tmaps
+find $compare_dir -name "comparison.tsv" > ${comparison}.all_comparisons
 echo -n "" > ${comparison}.tsv
-for f in `cat ${comparison}.all_tmaps`; do /bin/bash ./make_isoform_comparison_table.sh $f >> ${comparison}.tsv ; done
-sort -k1,1 ${comparison}.tsv > ${comparison}.sorted.tsv
+#for f in `cat ${comparison}.all_tmaps`; do /bin/bash ./make_isoform_comparison_table.sh $f >> ${comparison}.tsv ; done
+find $compare_dir -name "comparison.tsv" -exec /bin/bash -c "cat <(echo -n {} | cut -d'/' -f 2 | tr '\n' ' ') <(fgrep -v 'Shell' {})" \; >> ${comparison}.tsv
+fgrep "flipped" ${comparison}.tsv | sed -e 's/\.flipped//' > flipped
+cut -d' ' -f 1 flipped > flipped.names
+cat flipped <(fgrep -v -f flipped.names ${comparison}.tsv) | sort -k1,1 > ${comparison}.sorted.tsv
 
-paste <(cut -d' ' -f 1,2,3 ${comparison}.sorted.tsv) <(cut -d' ' -f 3 ${comparison}.sorted.tsv) <(cut -d' ' -f 4 ${comparison}.sorted.tsv) <(cut -d' ' -f 4 ${comparison}.sorted.tsv) > ${comparison}.sorted.pasted.tsv
+#paste <(cut -d' ' -f 1,2,3 ${comparison}.sorted.tsv) <(cut -d' ' -f 3 ${comparison}.sorted.tsv) <(cut -d' ' -f 4 ${comparison}.sorted.tsv) <(cut -d' ' -f 4 ${comparison}.sorted.tsv) > ${comparison}.sorted.pasted.tsv
--- gtf_compare/run_gcomf20.sh
+++ gtf_compare/run_gcomf20.sh
@@ -9,6 +9,20 @@ mkdir -p $dir
 cd $dir
 ln -fs ../${gtf1}
 ln -fs ../${gtf2}
-#../gcom -a -T -r $gtf1 --fuzz-length 20 $gtf2 > run 2>&1
 ../gcom -r $gtf1 --fuzz-length 20 $gtf2 > run 2>&1
+/bin/bash -x ../../make_isoform_comparison_table.sh gffcmp.${gtf2}.tmap > comparison.tsv
+#make backup of 1st run's generically named files
+#if pacbio NA12878 flip the strands of those transcripts which ended up in O/S categories
+FLIP_STRAND=`echo $dir | perl -ne '$n=$_; chomp($n); if($n=~/pb/) { print "1"; } else { print "0"; }'`
+if [[ "$FLIP_STRAND" == "1" ]]; then
+    egrep -e '	(o|s)	' gffcmp.${gtf2}.tmap.multi | cut -f 3 | sort -u  | sed -e 's/^\(.*\)$/"\1"/' > gffcmp.${gtf2}.tmap.multi.o_s
+    cat gffcmp.${gtf2}.tmap.multi.o_s $gtf2 | perl -ne 'chomp; $f=$_; @f=split(/\t/,$f); if(scalar @f == 1) { $f=~s/"//g; $h{$f}=1; next; } $f[8]=~/transcript_id "([^"]+)"/; $tid=$1; if($h{$tid}) { $s=$f[6]; $f[6]=($s eq "+"?"-":"+"); } print "".join("\t",@f)."\n";' > ${gtf2}.o_s_strand_flipped
+    mkdir ../${dir}.flipped
+    cd ../${dir}.flipped
+    ln -fs ../${gtf1}
+    ln -fs ../${dir}/${gtf2}.o_s_strand_flipped
+    #now re-run with flipped strands
+    ../gcom -r $gtf1 --fuzz-length 20 ${gtf2}.o_s_strand_flipped > run 2>&1
+    /bin/bash -x ../../make_isoform_comparison_table.sh gffcmp.${gtf2}.o_s_strand_flipped.tmap > comparison.tsv
+fi
 cd ../
